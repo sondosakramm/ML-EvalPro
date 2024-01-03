2b32a7522c6865d34b8175d68a11f33c2efe5147
sondosakramm <sondosakram51@gmail.com>
2024-01-03 04:45:33 +0200

    Evaluators Factory
    Adding EvaluatorsFactrory
    Delete summary package

--- auto_evaluator/evaluation_metrics/regression/regression_evaluator/regression_evaluator.py
+++ auto_evaluator/evaluation_metrics/regression/regression_evaluator/regression_evaluator.py
@@ -1,12 +1,8 @@
-from abc import abstractmethod
+from abc import ABC
 
 from auto_evaluator.evaluation_metrics.evaluation_metric import EvaluationMetric
 
 
-class RegressionEvaluator(EvaluationMetric):
+class RegressionEvaluator(EvaluationMetric, ABC):
     def __init__(self, target, prediction):
         super().__init__(target, prediction)
-
-    @abstractmethod
-    def measure(self):
-        pass
