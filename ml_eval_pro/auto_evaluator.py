1a6e35287525f6cb0c0f3403b58867b392e44808
ReemAyman <reemayman5299@gmail.com>
2024-05-07 11:51:26 +0300

    commenting transparency, change methods to protected

--- ml_eval_pro/auto_evaluator.py
+++ ml_eval_pro/auto_evaluator.py
@@ -11,6 +11,7 @@ from ml_eval_pro.gdpr.gdpr_rules.model_ethical import ModelEthical
 from ml_eval_pro.gdpr.gdpr_rules.model_reliability import ModelReliability
 from ml_eval_pro.gdpr.gdpr_rules.model_robustness import ModelRobustness
 from ml_eval_pro.gdpr.gdpr_rules.model_transparency import ModelTransparency
+from ml_eval_pro.model.evaluated_model_factory import EvaluatedModelFactory
 from ml_eval_pro.summary.modules_summary.bias_summary import BiasSummary
 from ml_eval_pro.summary.modules_summary.carbon_summary import CarbonSummary
 from ml_eval_pro.summary.modules_summary.inference_summary import InferenceSummary
@@ -19,7 +20,7 @@ from ml_eval_pro.summary.modules_summary.model_reliability_summary import ModelR
 from ml_eval_pro.summary.modules_summary.model_robustness_summary import ModelRobustnessSummary
 from ml_eval_pro.summary.modules_summary.model_transparency_summary import ModelTransparencySummary
 from ml_eval_pro.summary.modules_summary.variance_summary import VarianceSummary
-from ml_eval_pro.utils.validate_model_type import check_model_type, get_num_classes
+from ml_eval_pro.utils.validate_model_type import get_num_classes
 from ml_eval_pro.evaluation_metrics.evaluators_factory import EvaluatorsFactory
 from ml_eval_pro.variance.model_variance_by_test_data import ModelVarianceByTestData
 from ml_eval_pro.variance.model_variance_by_train_test_data import ModelVarianceByTrainTestData
@@ -27,23 +28,31 @@ from ml_eval_pro.variance.model_variance_by_train_test_data import ModelVariance
 
 class AutoEvaluator:
 
-    def __init__(self, model_pipeline, test_dataset: pd.DataFrame, test_target: pd.Series,
-                 evaluation_metrics: list, train_dataset: pd.DataFrame = None,
-                 train_target: pd.Series = None, features_description: dict = None):
+    def __init__(self, model_uri, model_type: str, test_dataset: pd.DataFrame, test_target: pd.Series,
+                 evaluation_metrics: list, features_description: dict, dataset_context: str,
+                 train_dataset: pd.DataFrame = None, train_target: pd.Series = None,
+                 spark_session=None, spark_feature_col_name="features"):
         """
         Create an instance of the auto evaluator to get all the evaluation metrics.
-        :param model_pipeline: the model pipeline.
+        :param model_uri: the model uri.
+        :param model_type: the model problem type.
         :param test_dataset: the test dataset features.
         :param test_target: the test dataset target.
         :param evaluation_metrics: the evaluation metrics to be calculated.
+        :param features_description: the features' description.
+        :param dataset_context: a description of the dataset context.
         :param train_dataset: the train dataset features.
         :param train_target: the train dataset target.
-        :param features_description: the features description,
-         where key is the feature and the value is the description.
+        :param spark_session: the spark session (used in spark models only).
+        :param spark_feature_col_name: the spark features column name (used in spark models only).
         :return: An instance of the auto evaluator.
         """
-        self.model_pipeline = model_pipeline
-        self.model_type = check_model_type(test_target)
+        self.spark_feature_col_name = spark_feature_col_name
+        self.model_pipeline = EvaluatedModelFactory.create(model_uri=model_uri, problem_type=model_type) \
+            if spark_session is None else EvaluatedModelFactory.create(model_uri=model_uri, problem_type=model_type,
+                                                                       spark_feature_col_name=spark_feature_col_name,
+                                                                       spark_session=spark_session)
+        self.model_type = model_type
         self.test_dataset = test_dataset
         self.test_target = test_target
         self.evaluation_metrics = evaluation_metrics
@@ -54,23 +63,24 @@ class AutoEvaluator:
         self.train_predictions = None if self.train_target is None \
             else self.model_pipeline.predict(self.train_dataset)
 
-        self.test_predictions_proba = None if self.model_type == "regression" \
-            else self.model_pipeline.predict_proba(self.test_dataset)
+        self.test_predictions_prob = None if self.model_type == "regression" \
+            else self.model_pipeline.predict(self.test_dataset, predict_class=False)
 
-        self.train_predictions_proba = None if self.model_type == "regression" or self.train_dataset is None \
-            else self.model_pipeline.predict_proba(self.train_dataset)
+        self.train_predictions_prob = None if self.model_type == "regression" or self.train_dataset is None \
+            else self.model_pipeline.predict(self.train_dataset, predict_class=False)
 
         self.num_classes = -1 if self.model_type == "regression" \
             else get_num_classes(self.model_type, self.test_target)
 
         self.features_description = features_description
+        self.dataset_context = dataset_context
 
-    def __get_evaluation_metrics(self, target, predictions, predictions_proba):
+    def _get_evaluation_metrics(self, target, predictions, predictions_prob):
         """
         Calculating the evaluation metrics.
         :param target: the target true values.
         :param predictions: the target prediction values.
-        :param predictions: the target prediction probability values of each class (for classification only).
+        :param predictions_prob: the target prediction probability values of each class (for classification only).
         :return: A dictionary of the values.
         """
         print("Evaluating the model using the input evaluation metrics ...")
@@ -78,7 +88,7 @@ class AutoEvaluator:
         for metric in self.evaluation_metrics:
             if metric == 'expected calibration error' or metric == 'auc':
                 res[metric] = EvaluatorsFactory.get_evaluator(metric, target,
-                                                              predictions_proba,
+                                                              predictions_prob,
                                                               num_of_classes=self.num_classes).measure()
             else:
                 res[metric] = EvaluatorsFactory.get_evaluator(metric, target,
@@ -95,18 +105,18 @@ class AutoEvaluator:
         return EvaluatorsFactory.get_evaluator(f"{self.model_type} reliability evaluation",
                                                self.test_target,
                                                self.test_predictions if self.model_type == 'regression' else
-                                               self.test_predictions_proba,
+                                               self.test_predictions_prob,
                                                num_of_classes=self.num_classes).measure()
 
-    def __get_environmental_impact(self) -> dict:
+    def _get_environmental_impact(self) -> dict:
         """
         Getting the environmental impact values.
         :return: a dictionary of the time inference and the carbon emission values.
         """
         print("Evaluating the model environmental impact ...")
         inference_time = InferenceTime(self.model_pipeline, self.test_dataset)
-        inference_time_val = (inference_time.calc_inference_time_hours()
-                              + inference_time.calc_inference_time_minutes()
+        inference_time_val = (inference_time.calc_inference_time_hours() * 360
+                              + inference_time.calc_inference_time_minutes() * 60
                               + inference_time.calc_inference_time_seconds())
 
         carbon_emission = Carbon(self.model_pipeline, self.test_dataset)
@@ -114,7 +124,6 @@ class AutoEvaluator:
 
         carbon_emission_carbon_per_prediction = carbon_emission_calc.calculate_carbon()
         carbon_emission_predictions_per_kg_co = carbon_emission_calc.calculate_predictions()
-
 
         return {
             'inference_time_val': inference_time_val,
@@ -124,7 +133,7 @@ class AutoEvaluator:
             'carbon_summary': CarbonSummary(carbon_emission).get_summary()
         }
 
-    def __get_model_bias(self) -> dict:
+    def _get_model_bias(self) -> dict:
         """
         Calculating the features bias in the model
         :return: a dictionary of the biased features and summary in the model.
@@ -134,11 +143,11 @@ class AutoEvaluator:
         biased_features = model_bias()
 
         return {
-            "biased_features": np.array(biased_features)[:, 0],
+            "biased_features": [] if len(biased_features) == 0 else np.array(biased_features)[:, 0],
             "bias_summary": BiasSummary(model_bias).get_summary()
         }
 
-    def __get_model_variance(self):
+    def _get_model_variance(self):
         """
         Calculating the model variance.
         :return: the model variance and its summary.
@@ -158,13 +167,14 @@ class AutoEvaluator:
         variance_res['variance_features_summary'] = VarianceSummary(variance_test).get_summary()
         return variance_res
 
-    def __get_feature_importance(self) -> dict:
+    def _get_feature_importance(self) -> dict:
         """
         Calculating the feature importance values in evaluating the model.
         :return: a dictionary of the biased features and summary in the model.
         """
         print("Evaluating the model ethical issues according to the features importance ...")
-        ethical_analysis = EthicalAnalysis(self.model_pipeline, self.test_dataset, self.features_description)
+        ethical_analysis = EthicalAnalysis(self.model_pipeline, self.test_dataset,
+                                           self.features_description, self.dataset_context)
         feature_importance, unethical_features = ethical_analysis()
 
         return {
@@ -172,7 +182,7 @@ class AutoEvaluator:
             'feature_ethnicity': unethical_features
         }
 
-    def __get_adversarial_test_cases(self) -> pd.DataFrame:
+    def _get_adversarial_test_cases(self) -> pd.DataFrame:
         """
         Getting the adversarial test cases of the model.
         :return: a data frame containing the adversarial test cases,
@@ -203,6 +213,7 @@ class AutoEvaluator:
             adversarial_examples_predictions)
 
         dataset_columns.extend(["Expected Output", "Model Output"])
+
         # if the model is robust, return an empty dataframe
         if true_value.size == 0:
             return pd.DataFrame(columns=dataset_columns)
@@ -213,17 +224,16 @@ class AutoEvaluator:
 
         return pd.DataFrame(adv_test_cases_instances, columns=dataset_columns)
 
-
-    def __get_model_gdpr_compliance(self):
+    def _get_model_gdpr_compliance(self):
         """
         Evaluating the model GDPR Compliance.
         :return: the model GDPR Compliance summary.
         """
         print("Evaluating the model GDPR Compliance ...")
 
-        model_ethical = 'Cannot evaluate model ethnicity due to the unavailability of the features.' if (
-                self.features_description is None) \
-            else ModelEthical(features_description=self.features_description)
+        model_ethical = ModelEthical(features_description=self.features_description,
+                                     dataset_context=self.dataset_context,
+                                     X_test=self.test_dataset)
 
         model_reliability = ModelReliability(model=self.model_pipeline,
                                              X_test=self.test_dataset,
@@ -236,17 +246,21 @@ class AutoEvaluator:
                                            y_test=self.test_target,
                                            problem_type=self.model_type)
 
-        model_transparency = ModelTransparency(model=self.model_pipeline,
-                                               X_test=self.test_dataset,
-                                               y_test=self.test_target,
-                                               problem_type=self.model_type)
+        # model_transparency = ModelTransparency(model=self.model_pipeline,
+        #                                        X_test=self.test_dataset,
+        #                                        y_test=self.test_target,
+        #                                        problem_type=self.model_type)
 
-        return (ModelEthicalSummary(model_ethical).get_summary() +
-                '\n' + ModelReliabilitySummary(model_reliability).get_summary() +
-                '\n' + ModelRobustnessSummary(model_robustness).get_summary() +
-                '\n' + ModelTransparencySummary(model_transparency).get_summary())
+        return (ModelReliabilitySummary(model_reliability).get_summary() +
+                '\n' + ModelEthicalSummary(model_ethical).get_summary() +
+                '\n' + ModelRobustnessSummary(model_robustness).get_summary())
 
-    def __get_machine_unlearning_ability(self):
+        # return(ModelReliabilitySummary(model_reliability).get_summary() +
+        #       '\n' + ModelEthicalSummary(model_ethical).get_summary() +
+        #       '\n' + ModelRobustnessSummary(model_robustness).get_summary() +
+        #       '\n' + ModelTransparencySummary(model_transparency).get_summary())
+
+    def _get_machine_unlearning_ability(self):
         """
         Evaluating the model machine unlearning ability.
         :return: the model machine unlearning ability summary.
@@ -260,10 +274,10 @@ class AutoEvaluator:
         """
         return {
             'evaluation_metrics_test': self.__get_evaluation_metrics(self.test_target, self.test_predictions,
-                                                                     self.test_predictions_proba),
+                                                                     self.test_predictions_prob),
 
             'evaluation_metrics_train': {} if self.train_dataset is None
-            else self.__get_evaluation_metrics(self.train_target, self.train_predictions, self.train_predictions_proba),
+            else self.__get_evaluation_metrics(self.train_target, self.train_predictions, self.train_predictions_prob),
 
             'reliability_diagram': self.__get_reliability_diagram(),
 
@@ -279,6 +293,6 @@ class AutoEvaluator:
 
             'gdpr_compliance': self.__get_model_gdpr_compliance(),
 
-            'machine_unlearning': self.__get_machine_unlearning_ability()
+            # 'machine_unlearning': self.__get_machine_unlearning_ability()
 
         }
